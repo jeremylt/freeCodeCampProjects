874f79d88d4487e736f83be1bccc1abe76174c09
Jeremy L Thompson <jeremy@jeremylt.org>
2022-02-08 15:35:11 -0700

    Update 00-mean-variance-standard-deviation-calculator.py

--- Data-Analysis-with-Python/00-mean-variance-standard-deviation-calculator.py
+++ Data-Analysis-with-Python/00-mean-variance-standard-deviation-calculator.py
@@ -4,56 +4,49 @@ import numpy as np
 def calculate(list):
     if len(list) < 9:
         raise ValueError("List must contain nine numbers.")
-    data = np.array([list[0:3], list[3:6], list[6:9]])
+    data = np.array(list).reshape(3,3)
     computations = {}
 
     # mean
-    mean = [
+    computations["mean"] = [
         np.mean(data, axis=0).tolist(),
         np.mean(data, axis=1).tolist(),
         np.mean(data),
     ]
-    computations["mean"] = mean
 
     # variance
-    variance = [
+    computations["variance"] = [
         np.var(data, axis=0).tolist(),
         np.var(data, axis=1).tolist(),
         np.var(data),
     ]
-    computations["variance"] = variance
 
     # standard deviation
-    std_dev = [
+    computations["standard deviation"] = [
         np.std(data, axis=0).tolist(),
         np.std(data, axis=1).tolist(),
         np.std(data),
     ]
-    computations["standard deviation"] = std_dev
 
     # max
-    max = [
+    computations["max"] = [
         np.max(data, axis=0).tolist(),
         np.max(data, axis=1).tolist(),
         np.max(data),
     ]
-    computations["max"] = max
 
     # min
-    min = [
+    computations["min"] = [
         np.min(data, axis=0).tolist(),
         np.min(data, axis=1).tolist(),
         np.min(data),
     ]
-    computations["min"] = min
 
     # sum
-    sum = [
+    computations["sum"] = [
         np.sum(data, axis=0).tolist(),
         np.sum(data, axis=1).tolist(),
         np.sum(data),
     ]
-    computations["sum"] = sum
 
     return computations
-
