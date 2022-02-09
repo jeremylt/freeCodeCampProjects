4488b8af1f3e9b5beb01722c4e6d91fb813937c8
Jeremy L Thompson <jeremy@jeremylt.org>
2022-02-08 19:47:16 -0700

    Update 02-caesars-cipher.js

--- JavaScript-Algorithms-and-Data-Structures/02-caesars-cipher.js
+++ JavaScript-Algorithms-and-Data-Structures/02-caesars-cipher.js
@@ -1,8 +1,9 @@
+// Constants
+const SHIFT = 13;
+const CODE_A = "A".charCodeAt(0);
+const CODE_Z = "Z".charCodeAt(0);
+
 function rot13(str) {
-  // Constants
-  const SHIFT = 13;
-  const CODE_A = "A".charCodeAt(0);
-  const CODE_Z = "Z".charCodeAt(0);
   // Map old characters to encoded (or back since this is a shift of 13)
   return str
     .split("")
