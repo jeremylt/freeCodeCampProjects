d3d5ef25ddeda6f48dd94e76f743106a50fd1431
Jeremy L Thompson <jeremy@jeremylt.org>
2025-07-03 13:29:40 -0600

    Update 05-creature-search-app.js

--- JavaScript-Algorithms-and-Data-Structures-New/05-creature-search-app.js
+++ JavaScript-Algorithms-and-Data-Structures-New/05-creature-search-app.js
@@ -2,64 +2,44 @@ const creatureApiUrl = "https://rpg-creature-api.freecodecamp.rocks/api/creature
 
 // Get value for stat, if present
 const getPokemonStatValue = (data, stat) => {
-  if (!data.stats) return;
+  if (!data.stats) return '';
   for (const entry of data.stats) {
-    if (entry.name === stat) {
-      return entry.base_stat;
-    }
+    if (entry.name === stat) return entry.base_stat;
   }
 }
 
 // Update display for current Creature
 const showCreature = (data) => {
   // Attributes pre-pendend with 'creature-'
-  {
-    document.getElementById('creature-name').innerText = data.name.toUpperCase();
-    document.getElementById('creature-id').innerText = '#' + data.id;
-  }
+  document.getElementById('creature-name').innerText = (data.name || '').toUpperCase();
+  document.getElementById('creature-id').innerText = data.id || '';
   // Attributes where name matches span
-  {
-    const attributes = [
-      'weight',
-      'height',
-    ]
-    for (const attribute of attributes) {
-      document.getElementById(attribute).innerText = data[attribute];
-    }
-  }
+  [
+    'weight',
+    'height',
+  ]
+    .forEach(attribute => document.getElementById(attribute).innerText = data[attribute] || '');
   // Stat attributes
-  {
-    const stats = [
-      'hp',
-      'attack',
-      'defense',
-      'special-attack',
-      'special-defense',
-      'speed',
-    ]
-    for (const stat of stats) {
-      const value = getPokemonStatValue(data, stat);
-
-      document.getElementById(stat).innerText = value ? value : '';
-    }
-  }
+  [
+    'hp',
+    'attack',
+    'defense',
+    'special-attack',
+    'special-defense',
+    'speed',
+  ]
+    .forEach(stat => document.getElementById(stat).innerText = getPokemonStatValue(data, stat));
   // Types
-  document.getElementById('types').innerHTML = "";
-  if (!data.types) return;
-  for (const type of data.types) {
-    document.getElementById('types').innerHTML += `<span> ${type.name.toUpperCase()}</span>`;
-  }
+  document.getElementById('types').innerHTML = data.types ?
+    data
+      .types
+      .map(type => `<span>${type.name.toUpperCase()}</span>`)
+      .join(" ")
+    : "";
 }
 
 // Clear current display
-const clearCreature = () => {
-  showCreature({
-    name: '',
-    id: '',
-    weight: '',
-    height: '',
-  });
-}
+const clearCreature = () => showCreature({});
 
 // Fetch pokemon and display
 const fetchCreature = (search) => {
@@ -67,7 +47,7 @@ const fetchCreature = (search) => {
     .then(res => res.json())
     .then(data => showCreature(data))
     .catch(err => {
-      console.log(err);
+      console.log("Error fetching creature:", err);
       alert("Creature not found");
     });
 }
